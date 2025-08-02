853c53e47f30450041ccfd4a6adb74b4be895e46
ShivaniVenkataraman <ShivaniVenkatraman.TPA@jiostar.com>
2025-08-02 21:53:07 +0530

    React create element episode 2

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//React.createElement => creates a object => after render converts to HTML
 const heading = React.createElement('h1', {id: 'heading'}, 'Hello, World from React!');
+
+console.log(heading); // This will log the React element object
+// JSX - transpiled before JS - PARCEL - BABEL (JSX to JS) - React Element
+
+// Babel converts it into JSX => React.createElement => React Element JS - (Object) => Renders HTML Element
+const jsxHeading = <h1 id="heading">Hello, World from React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(jsxHeading);
