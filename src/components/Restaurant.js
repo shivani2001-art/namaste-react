800d293c9a66cb4f7f6b585d2c26f2ee6cea66fa
ShivaniVenkataraman <ShivaniVenkatraman.TPA@jiostar.com>
2025-08-12 13:03:07 +0530

    UseEffect hooks

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,16 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
-import ResList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect, use} from "react";
 
 const Restaurant = () => {
-    const [filteredRestaurants, setFilteredRestaurants] = useState(ResList);
+
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        // Simulate fetching data
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9520781&lng=80.2404265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+        console.log(json);
+        // Update state with fetched data
+        setFilteredRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    };
 
     return (
         <div className='restaurant'>
             <div className='search'>
                 <button onClick={() => {
-                    let filteredRestaurants = ResList.filter(restaurant => restaurant.info.avgRating > 4);
-                    setFilteredRestaurants(filteredRestaurants);
+                    let topRated = filteredRestaurants.filter(restaurant => restaurant.info.avgRating > 4);
+                    setFilteredRestaurants(topRated);
                 }}>Top Rated Restaurants</button>
             </div>
             <div className='restaurant-card'>
